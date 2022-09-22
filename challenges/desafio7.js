d6f45e0e082fe822683a3c67b4fa773ca24bcbfb
Janaina P de Oliveira Minutti <janainapp@gmail.com>
2022-09-22 17:02:04 -0300

    correct req 6 e 7

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,1 +1,2 @@
-db.produtos.find({ vendidos: { $ne: 50 }, tags: { $exists: 0 } }, { _id:0, vendidos: 1, nome: 1 });
+db.produtos.find({ vendidos: { $ne: 50 }, tags: { $exists: 0 } },
+    { _id: 0, vendidos: 1, nome: 1 });

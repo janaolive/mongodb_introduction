70ef23ff2d8d4bae32268e8e1496d535f2dc048e
Janaina P de Oliveira Minutti <janainapp@gmail.com>
2022-09-22 18:32:46 -0300

    req 12, 13 e 14

---
+++ challenges/desafio12.js
@@ -0,0 +1,9 @@
+db.produtos.updateMany({ 
+  nome: { $ne: "McChicken" },
+}, {
+  $addToSet: {
+    ingredientes: "ketchup",
+  },
+});
+
+db.produtos.find({}, { _id: 0, ingredientes: 1, nome: 1 });
